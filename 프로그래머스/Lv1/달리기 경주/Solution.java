98af72e9c5270b8c22580f92ac8ee414e08b0942
nigoleto <nigoleto@gmail.com>
2024-10-23 23:01:21 +0900

    241023_프로그래머스_1

--- 프로그래머스/Lv1/달리기 경주/Solution.java
+++ 프로그래머스/Lv1/달리기 경주/Solution.java
@@ -1,6 +1,25 @@
+import java.util.*;
+
 class Solution {
     public String[] solution(String[] players, String[] callings) {
-        String[] answer = {};
+
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < players.length; i++) {
+            map.put(players[i], i);   
+        };
+
+        for(String calling : callings) {
+            map.get(calling)
+        }
+        
+        String[] answer = new String[map.size()];
+        
+        for(String i: map.keySet()) {
+            answer[map.get(i)] = i;
+        }
+
+        
         return answer;
     }
 }
