f66d0e864b28aafe5866cbacfd9209a2f474dadf
nigoleto <nigoleto@gmail.com>
2024-10-25 13:44:26 +0900

    241025_프로그래머스_1_Update Solution.java

--- 프로그래머스/Lv1/달리기 경주/Solution.java
+++ 프로그래머스/Lv1/달리기 경주/Solution.java
@@ -3,14 +3,37 @@ import java.util.*;
 class Solution {
     public String[] solution(String[] players, String[] callings) {
 
+        for(int i = 0; i < callings.length; i++){
+            int idx = 0;
+            String ori = "";
+            for(int j = 0; j < players.length; j++){
+                if(callings[i].equals(players[j])){
+                    idx = j;
+                }
+            }
+            ori = players[idx];
+            players[idx] = players[idx-1];
+            players[idx-1] = ori;
+        }
+        return players;
+    }
+}
+
+// --------------------------- 시간초과를 해결하기 위해서 Hashmap사용 -----------------------
+
+class Solution {
+    public String[] solution(String[] players, String[] callings) {
+
         HashMap<String, Integer> map = new HashMap<>();
         
         for(int i = 0; i < players.length; i++) {
             map.put(players[i], i);   
         };
 
+        int index = 0;
+        
         for(String calling : callings) {
-            map.get(calling)
+            index = map.get(calling);
         }
         
         String[] answer = new String[map.size()];
@@ -19,6 +42,5 @@ class Solution {
             answer[map.get(i)] = i;
         }
         
-        return answer;
     }
 }
