bcca45842cccd94994e2d554d4ee3052a775012c
nigoleto <nigoleto@gmail.com>
2024-10-27 23:47:48 +0900

    241027_프로그래머스_1_바탕화면 정리

--- 프로그래머스/Lv1/바탕화면 정리/Solution.java
+++ 프로그래머스/Lv1/바탕화면 정리/Solution.java
@@ -1,6 +1,18 @@
 class Solution {
     public int[] solution(String[] wallpaper) {
-        int[] answer = {};
+        int[] answer = new int[4];
+        answer[0] = wallpaper.length;
+        answer[1] = wallpaper[0].length();
+        
+        for(int i = 0; i < wallpaper.length; i++) {
+            if(wallpaper[i].contains("#")) {
+                answer[0] = Math.min(answer[0], i);
+                answer[1] = Math.min(answer[1], wallpaper[i].indexOf('#'));
+                answer[2] = Math.max(answer[2], i+1);
+                answer[3] = Math.max(answer[3], wallpaper[i].lastIndexOf('#') + 1);
+            }
+        }
+        
         return answer;
     }
 }
