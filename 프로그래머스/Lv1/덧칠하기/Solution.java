3554fd7165c65a3c253040b55b8c82f4fe1a58bf
nigoleto <nigoleto@gmail.com>
2024-10-28 22:36:42 +0900

    프로그래머스_1_덧칠하기

--- 프로그래머스/Lv1/덧칠하기/Solution.java
+++ 프로그래머스/Lv1/덧칠하기/Solution.java
@@ -15,4 +15,20 @@ class Solution {
         
         return answer;
     }
+
+
+    // --------다른사람 풀이
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        int k = 1;
+        
+        for(int i = 0; i < section.length; i++){
+            if(section[j] >= k) {
+                k = m + section[i];
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
 }
