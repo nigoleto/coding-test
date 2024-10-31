4d9afcdafe6c3146b26378b0af69454c0dfea61f
nigoleto <nigoleto@gmail.com>
2024-11-01 01:25:15 +0900

    프로그래머스_1_둘만의 암호

---
+++ 프로그래머스/Lv1/둘만의 암호/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+
+        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+
+        for(int i = 0; i < alphabet.length; i++) {
+            alphabet[i]
+        }
+
+        return answer;
+    }
+}
