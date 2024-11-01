53186f767f4d5b3c51002b4d4691392f1faee418
nigoleto <nigoleto@gmail.com>
2024-11-01 23:58:47 +0900

    프로그래머스_1_둘만의 암호

--- 프로그래머스/Lv1/둘만의 암호/Solution.java
+++ 프로그래머스/Lv1/둘만의 암호/Solution.java
@@ -1,13 +1,20 @@
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
-
-        char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
-
-        for(int i = 0; i < alphabet.length; i++) {
-            alphabet[i]
+        
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        
+        alphabet = alphabet.replaceAll("[" + skip + "]", "");
+        
+        for(int i = 0; i < s.length(); i++) {
+            int curIdx = alphabet.indexOf(s.charAt(i));
+            int newIdx = (curIdx + index) % alphabet.length();
+            answer += alphabet.charAt(newIdx);
         }
-
+        
         return answer;
     }
 }
+
+// replaceAll로 skip에 들어가는 알파벳 빼기
+// 인덱스가 알파벳 숫자 이상으로 넘어가면 A로 다시 돌아갈 수 있게 % 사용
