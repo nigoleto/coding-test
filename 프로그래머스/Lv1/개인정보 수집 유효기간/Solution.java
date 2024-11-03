b357124e6b6cddfcab4b4a7cd91e608867e9d3d8
nigoleto <nigoleto@gmail.com>
2024-11-03 23:33:02 +0900

    프로그래머스_1_개인정보 수집 유효기간

--- 프로그래머스/Lv1/개인정보 수집 유효기간/Solution.java
+++ 프로그래머스/Lv1/개인정보 수집 유효기간/Solution.java
@@ -1,6 +1,46 @@
+import java.util.*;
+
 class Solution {
-    public int[] solution(String today, String[] terms, String[] privacies) {
-        int[] answer = {};
+    public List<Integer> solution(String today, String[] terms, String[] privacies) {
+        List<Integer> answer = new ArrayList<>();
+        HashMap<String, Integer> map = new HashMap<>();
+        
+        int nowDate = Integer.parseInt(today.split("\\.")[0])*10000 +
+            Integer.parseInt(today.split("\\.")[1])*100 +
+            Integer.parseInt(today.split("\\.")[2]);
+        
+        
+        for(int i = 0; i < terms.length; i++) {
+             map.put(terms[i].split(" ")[0], Integer.parseInt(terms[i].split(" ")[1]));
+        }
+        
+        
+        for(int i = 0; i < privacies.length; i++) {
+            int addMonth = (int)map.get(privacies[i].split(" ")[1]);
+            
+            int[] addDate = new int[3];
+            
+            addDate[0] = Integer.parseInt(privacies[i].substring(0,4));
+            addDate[1] = Integer.parseInt(privacies[i].substring(5,7)) + addMonth;
+            addDate[2] = Integer.parseInt(privacies[i].substring(8,10)) - 1;
+            
+            if(addDate[1] > 12) {
+                addDate[0] += (addDate[1] - 1) / 12;
+                addDate[1] = (addDate[1] - 1) % 12 + 1;
+            }
+            
+            if(addDate[2] == 0) {
+                addDate[2] = 28;
+                addDate[1]--;
+            }
+            
+            int resultDate = addDate[0]*10000 + addDate[1]*100 + addDate[2];
+            
+            if(resultDate > nowDate) {
+               answer.add(i+1); 
+            }
+        }
+        
         return answer;
     }
 }
