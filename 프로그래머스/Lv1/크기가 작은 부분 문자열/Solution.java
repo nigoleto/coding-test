31505c13024517f8f7fe688a1f13ec3970ef2fe5
nigoleto <nigoleto@gmail.com>
2024-11-04 23:51:33 +0900

    프로그래머스_1_크기가 작은 부분 문자열

--- 프로그래머스/Lv1/크기가 작은 부분 문자열/Solution.java
+++ 프로그래머스/Lv1/크기가 작은 부분 문자열/Solution.java
@@ -1,6 +1,19 @@
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
+        
+        //나눌 수
+        int div = p.length();
+        
+        for(int i = 0; i <= t.length() - div; i++) {
+            if(Long.parseLong(t.substring(i,i + div)) <= Long.parseLong(p)) {
+                answer++;   
+            }
+        }
+        
         return answer;
     }
 }
+
+// 숫자가 크기 때문에 Integer.parseInt로는 사용 불가
+// Long.parseLong사용
