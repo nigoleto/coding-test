d2e6311d85f7c4746f79970288e4fc7e80194b63
nigoleto <nigoleto@gmail.com>
2024-11-05 00:00:18 +0900

    프로그래머스_1_가장 가까운 같은 글자

---
+++ 프로그래머스/Lv1/가장 가까운 같은 글자/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] solution(String s) {
+        int[] answer = new int[s.length()];
+        
+        // for(int i = 0; i < s.length(); i++) {
+        //     char word = s.charAt(i);
+        //     answer[i] = s.lastIndexOf(word);
+        // }
+        
+        return answer;
+    }
+}
