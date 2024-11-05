b8fbd7ea9764637b63b91f10690c5a4bc7dc0628
nigoleto <nigoleto@gmail.com>
2024-11-05 16:08:03 +0900

    프로그래머스_1_가장 가까운 같은 글자

--- 프로그래머스/Lv1/가장 가까운 같은 글자/Solution.java
+++ 프로그래머스/Lv1/가장 가까운 같은 글자/Solution.java
@@ -2,10 +2,13 @@ class Solution {
     public int[] solution(String s) {
         int[] answer = new int[s.length()];
         
-        // for(int i = 0; i < s.length(); i++) {
-        //     char word = s.charAt(i);
-        //     answer[i] = s.lastIndexOf(word);
-        // }
+        for(int i = 0; i < s.length(); i++) {
+            char word = s.charAt(i);
+            String words = s.substring(0,i);
+            int result = words.lastIndexOf(word);
+            
+            answer[i] = result >= 0 ? i - result : result;
+        }
         
         return answer;
     }
