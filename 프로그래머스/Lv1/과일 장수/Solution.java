5a95bfbdb739abcb4a2c0c188f355cb64df5abfc
nigoleto <nigoleto@gmail.com>
2024-11-08 00:24:35 +0900

    프로그래머스_1_과일 장수_추가

--- 프로그래머스/Lv1/과일 장수/Solution.java
+++ 프로그래머스/Lv1/과일 장수/Solution.java
@@ -20,3 +20,17 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 사람의 풀이
+
+public int solution(int k, int m, int[] score) {
+    int answer = 0;
+
+    Arrays.sort(score);
+            
+    for(int i = score.length; i >= m; i -= m) {
+        answer += score[i-m] * m;
+    }
+
+    return answer;
+}
