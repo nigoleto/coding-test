d4ddcde3785f4ba41995c51504ffc3b555cafe37
nigoleto <nigoleto@gmail.com>
2024-11-07 22:35:30 +0900

    프로그래머스_1_기사단원의 무기

--- 프로그래머스/Lv1/기사단원의 무기/Solution.java
+++ 프로그래머스/Lv1/기사단원의 무기/Solution.java
@@ -19,3 +19,27 @@ class Solution {
 }
 
 // 위의 코드는 시간초과가 나온다. 이중for문 때문에
+
+class Solution {
+    public int solution(int number, int limit, int power) {
+        int answer = 1;
+        
+        for(int i = 2; i <= number; i++) {
+            int count = 0;
+            
+            for(int j = 1; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    if (j == i / j) { 
+                        count += 1;
+                    } else {
+                        count += 2;
+                    }
+                }
+            }
+            
+            answer += count > limit ? power : count;
+        }
+        
+        return answer;
+    }
+}
