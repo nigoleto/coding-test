101ae3d777a67b86738ab0dc8e29ccb3e173f678
nigoleto <nigoleto@gmail.com>
2024-11-10 23:30:45 +0900

    프로그래머스_1_햄버거 만들기

--- 프로그래머스/Lv1/햄버거 만들기/Solution.java
+++ 프로그래머스/Lv1/햄버거 만들기/Solution.java
@@ -1,6 +1,47 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
+//         String word = "";
+        
+//         for(int i: ingredient) {
+//             word += i;
+//         }
+        
+//         while(word.contains("1231")) {
+//             word = word.replace("1231","");
+//             answer++;
+//         }
+
+// 위 코드는 시간초과
+        
+        
+        List<Integer> arrayList = Arrays.stream(ingredient)
+                                        .boxed()
+                                        .collect(Collectors.toList()); 
+        
+
+        for(int i = 0; i < arrayList.size() - 3; i++) {
+            if(arrayList.get(i) == 1) {
+                if(arrayList.get(i+1) == 2) {
+                    if(arrayList.get(i+2) == 3) {
+                        if(arrayList.get(i+3) == 1) {
+                            answer++;
+                            arrayList.remove(i+3);
+                            arrayList.remove(i+2);
+                            arrayList.remove(i+1);
+                            arrayList.remove(i);
+                            if(i - 4 < 0) {
+                                i = -1;
+                            } else i -= 4;
+                        }
+                    }
+                }
+            }
+        }
+        
         return answer;
     }
 }
