979d2c62311973da6e2ac89b3f745629c0eccfa9
nigoleto <nigoleto@gmail.com>
2024-11-18 23:47:12 +0900

    프로그래머스_1_숫자 문자열과 영단어

---
+++ 프로그래머스/Lv1/숫자 문자열과 영단어/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String s) {
+
+        String[] nums = {"zero","one","two","three","four","five","six","seven","eight","nine", "ten"};
+        
+        for(int i = 0; i < nums.length; i++) {
+            s = s.replace(nums[i], "" + i);
+        }
+        
+        return Integer.parseInt(s);
+    }
+}
