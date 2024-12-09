32f1d50807589cf5c29ceda31c84d89a71e0cd28
nigoleto <nigoleto@gmail.com>
2024-12-09 22:48:52 +0900

    프로그래머스_1_음양더하기

---
+++ 프로그래머스/Lv1/음양 더하기/Solution.java
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// absolutes_len은 배열 absolutes의 길이입니다.
+// signs_len은 배열 signs의 길이입니다.
+int solution(int absolutes[], size_t absolutes_len, bool signs[], size_t signs_len) {
+    int answer = 0;
+    
+    for(int i = 0; i < absolutes_len; i++) {
+        answer = signs[i] ? answer + absolutes[i] : answer - absolutes[i];
+    }
+    return answer;
+}
