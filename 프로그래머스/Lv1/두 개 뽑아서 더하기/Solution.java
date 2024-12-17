2e83a5f1390538df324c8b32f214006c076d13ab
nigoleto <nigoleto@gmail.com>
2024-12-17 09:38:13 +0900

    프로그래머스_1_두 개 뽑아서 더하기

---
+++ 프로그래머스/Lv1/두 개 뽑아서 더하기/Solution.java
@@ -0,0 +1,16 @@
+import java.util.TreeSet;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int[] answer = {};
+
+        TreeSet<Integer> set = new TreeSet<>();
+        
+        for(int i =0; i < numbers.length; i++) {
+            for(int j = i+1; j < numbers.length; j++){
+                set.add(numbers[i] + numbers[j]);
+            }
+        }
+        return set.stream.mapToInt(Integer::intValue).toArray();
+    }
+}
