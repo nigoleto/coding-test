5e78ba7d87f57c689de889607e6d5ef9f7671c5e
nigoleto <nigoleto@gmail.com>
2025-02-04 22:45:49 +0900

    프로그래머스_1_문자열 내림차순으로 배치하기

---
+++ 프로그래머스/Lv1/문자열 내림차순으로 배치하기/Solution.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public String solution(String s) {
+        StringBuilder answer = new StringBuilder();
+        String[] list = s.split("");
+        Arrays.sort(list, Collections.reverseOrder());
+        for(String str: list) {
+            answer.append(str);
+        }
+        return answer.toString();
+    }
+}
