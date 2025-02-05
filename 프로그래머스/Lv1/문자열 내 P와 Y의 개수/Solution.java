f3880238e01742302c97e8788a4c051095e50bd9
nigoleto <nigoleto@gmail.com>
2025-02-05 23:51:56 +0900

    프로그래머스_1_문자열 내 P와 Y의 개수

--- 프로그래머스/Lv1/문자열 내 P와 Y의 개수/Solution.java
+++ 프로그래머스/Lv1/문자열 내 P와 Y의 개수/Solution.java
@@ -1,12 +1,9 @@
 class Solution {
     boolean solution(String s) {
-        boolean answer = true;
 
         int countP = s.toUpperCase().replaceAll("[^P]", "").length();
         int countY = s.toUpperCase().replaceAll("[^Y]", "").length();
         
-        answer = countP == countY ? true : false;
-        
-        return answer;
+        return countP == countY;
     }
 }
