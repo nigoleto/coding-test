6c8eec8a1e2170757d377aff457b4bee585e5cf4
nigoleto <nigoleto@gmail.com>
2025-02-09 23:28:47 +0900

    프로그래머스_1_문자열 내 마음대로 정리하기

--- 프로그래머스/Lv1/문자열 내 마음대로 정리하기/Solution.java
+++ 프로그래머스/Lv1/문자열 내 마음대로 정리하기/Solution.java
@@ -8,5 +8,13 @@ class Solution {
         }
         
         return answer.stream().sorted().map(i -> i.substring(1)).toArray(String[]::new);
+
+        //이게 더 빠르긴함
+        // Collections.sort(answer);
+        // String[] answer2 = new String[answer.size()];
+        // for (int i = 0; i < answer.size(); i++) {
+        //     answer2[i] = answer.get(i).substring(1);
+        // }
+        // return answer2;
     }
 }
