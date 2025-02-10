9b1c9410a4fa2acf6aa36e087226a4bf3c9d8e0b
nigoleto <nigoleto@gmail.com>
2025-02-10 22:52:43 +0900

    프로그래머스_1_최대공약수와 최소공배수

--- 프로그래머스/Lv1/최대공약수와 최소공배수/Solution.java
+++ 프로그래머스/Lv1/최대공약수와 최소공배수/Solution.java
@@ -24,3 +24,37 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이
+class Solution2 {
+    public int[] solution2(int n, int m) {
+
+        int[] answer = new int[2];
+        
+        for (int i = 1; i <= Math.min(n, m); i++) {
+            if (Math.max(n, m) % i == 0 && Math.min(n, m) % i == 0) {
+                answer[0] = i;
+                answer[1] = n * m / answer[0];
+            }
+        }
+
+        return answer;
+    }
+}
+
+// 이게 더빠름
+class Solution3 {
+    public int[] solution3(int n, int m) {
+
+        int[] answer = new int[2];
+        
+        for (int i = 1; i < n + m; i++) {
+            if (n % i == 0 && m % i == 0) {
+                answer[0] = i;
+                answer[1] = n * m / answer[0];
+            }
+        }
+
+        return answer;
+    }
+}
