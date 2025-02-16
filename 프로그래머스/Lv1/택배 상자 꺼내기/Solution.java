27ed4ee72818805b47beebf07d6cff7bbb0851f6
nigoleto <nigoleto@gmail.com>
2025-02-16 23:29:42 +0900

    프로그래머스_1_택배 상자 꺼내기

---
+++ 프로그래머스/Lv1/택배 상자 꺼내기/Solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n, int w, int num) {
+        int answer = 0;
+        int max = n / w + 1;
+        
+
+        indexOf(num) % w
+            
+        return answer;
+    }
+}
