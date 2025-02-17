32ec29c2f1d5ef2e3a9c5883d9989060492eb134
nigoleto <nigoleto@gmail.com>
2025-02-17 23:04:03 +0900

    프로그래머스_1_택배 상자 꺼내기

--- 프로그래머스/Lv1/택배 상자 꺼내기/Solution.java
+++ 프로그래머스/Lv1/택배 상자 꺼내기/Solution.java
@@ -1,11 +1,19 @@
 class Solution {
     public int solution(int n, int w, int num) {
-        int answer = 0;
-        int max = n / w + 1;
+        int max = (n - 1) / w + 1;
+        int count = max - ((num - 1) / w);
+        int curIdx = direction(num, w) ? (num - 1) % w : w - 1 - ((num - 1) % w);
+        int maxIdx = direction(n, w) ? (n - 1) % w : w - 1 - ((n - 1) % w);
         
-
-        indexOf(num) % w
-            
-        return answer;
+        if(direction(n, w)) {
+            return maxIdx < curIdx ? count - 1 : count; 
+        } else {
+            return maxIdx > curIdx ? count - 1 : count; 
+        }
+    }
+    
+    // true면 정방향 false면 역방향
+    public boolean direction(int number, int w) {
+        return (number - 1) / w % 2 == 0; 
     }
 }
