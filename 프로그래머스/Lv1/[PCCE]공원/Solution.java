17d5b9f2c7dd80a17a4fa08561747df57419397f
nigoleto <nigoleto@gmail.com>
2025-02-20 22:24:44 +0900

    프로그래머스_1_[PCCE]공원

--- 프로그래머스/Lv1/[PCCE]공원/Solution.java
+++ 프로그래머스/Lv1/[PCCE]공원/Solution.java
@@ -1,6 +1,32 @@
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
+        int answer = -1;
+        
+        for (int k = mats.length - 1; k >= 0; k--) {
+            int mat = mats[k];
+            for (int i = 0; i <= park.length - mat; i++) {
+                for(int j = 0; j <= park[0].length - mat; j++) {
+                    if (check(i, j, mat, park)) {
+                        answer = Math.max(mat, answer);
+                    }
+                }
+            }
+        }
+        
         return answer;
+    }
+    
+    public boolean check(int x, int y, int size, String[][] park) {
+        
+        if (x + size > park.length || y + size > park[0].length) return false;
+        for (int i = x; i < x + size; i++) {
+            for(int j = y; j < y + size; j++) {
+                if(!park[i][j].equals("-1")) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
     }
 }
