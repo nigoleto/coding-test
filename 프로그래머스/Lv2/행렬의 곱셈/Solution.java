9c4d8acea6c462e48c7edbb2a9e7cc469d9430c4
nigoleto <nigoleto@gmail.com>
2025-03-08 23:55:32 +0900

    Update Solution.java

--- 프로그래머스/Lv2/행렬의 곱셈/Solution.java
+++ 프로그래머스/Lv2/행렬의 곱셈/Solution.java
@@ -1,6 +1,22 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
-        int[][] answer = {};
+        int[][] answer = new int[arr1.legnth][arr2[0].length];
+        
+        1 4
+        3 2   3 3
+        4 1   3 3
+        
+        for (int i = 0; i < arr1.length; i++) {
+            for(int j = 0; j < arr2[0].length; j++) {
+                
+                for(int k = 0; k < arr1[0].legnth; k++) {
+                    
+                }
+                
+            }
+        }
+        
+        return answer;
         return answer;
     }
 }
