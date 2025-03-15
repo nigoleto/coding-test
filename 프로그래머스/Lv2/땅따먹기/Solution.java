df454d27b5d66d6c832138a8b7a21ab9e982c674
nigoleto <nigoleto@gmail.com>
2025-03-15 23:09:34 +0900

    프로그래머스_2_땅따먹기

--- 프로그래머스/Lv2/땅따먹기/Solution.java
+++ 프로그래머스/Lv2/땅따먹기/Solution.java
@@ -2,7 +2,7 @@ class Solution {
     int solution(int[][] land) {
         int answer = 0;
 
-        // [시도중]
+        // [시도중] -[실패]
 
         for (int i = 0; i < land.length - 1; i++) {
             int max1 = 0;
@@ -46,6 +46,57 @@ class Solution {
 
             }
         }
+        
+        return answer;
+    }
+}
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+            int max = 0;
+            int maxIdx = 0;
+            int second = 0;
+
+            for(int i = 0; i < land.length; i++) {
+
+                for(int j = 0; j < land[0].length; j++) {
+                    if (max < land[i][j]) {
+                        max = land[i][j];
+                        maxIdx = j;
+                    }
+                }
+
+                for(int j = 0; j < land[0].length; j++) {
+                    if (max = land[i][j]) {
+                        continue;
+                    } else if (second < land[i][j]) {
+                        second = land[i][j];
+                    }
+                }
+            }
+
+            for(int i = 0; i < land.length; i++) {
+                int maxTemp = 0;
+                int maxIdxTemp = 0;
+                int secondTemp = 0;
+
+                for(int j = 0; j < land[0].length; j++) {
+                    if (maxTemp < land[i][j]) {
+                        maxTemp = land[i][j];
+                        maxIdxTemp = j;
+                    }
+                }
+
+                for(int j = 0; j < land[0].length; j++) {
+                    if (maxTemp = land[i][j]) {
+                        continue;
+                    } else if (secondTemp < land[i][j]) {
+                        secondTemp = land[i][j];
+                    }
+                }
+            }
+
         
         return answer;
     }
