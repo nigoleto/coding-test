c2925b564006c8d622d939216251aa589970ad5f
nigoleto <nigoleto@gmail.com>
2025-03-30 23:17:23 +0900

    프로그래머스_2_서버 증설 횟수

--- 프로그래머스/Lv2/서버 증설 횟수/Solution.java
+++ 프로그래머스/Lv2/서버 증설 횟수/Solution.java
@@ -1,18 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
-        int count = k;
+        Map<Integer,Integer> map = new HashMap<>();
         int server = 1;
         
         for (int i = 0; i < players.length; i++) {
-            if (players[i] >= (server * m)) {
-                count += k;
-                server++;
-                answer++;
+            if (map.containsKey(i)) {
+                server -= map.get(i);
             }
-            if (count > 0 ) count --;
-            if (count == 0) {
-                server--;
+            
+            if (players[i] >= (server * m)) {
+                int value = (int) Math.ceil(((double)(players[i] + 1) - (server * m)) / (double)m);
+                server += value;
+                map.put(i+k, value);
+                answer += value;
             }
         }
         
