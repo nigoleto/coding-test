74d56922180bc093303f53faa92d91de88fcecfb
nigoleto <nigoleto@gmail.com>
2025-03-30 22:58:52 +0900

    프로그래머스_2_서버 증설 횟수_풀이중

--- 프로그래머스/Lv2/서버 증설 횟수/Solution.java
+++ 프로그래머스/Lv2/서버 증설 횟수/Solution.java
@@ -1,6 +1,21 @@
 class Solution {
     public int solution(int[] players, int m, int k) {
         int answer = 0;
+        int count = k;
+        int server = 1;
+        
+        for (int i = 0; i < players.length; i++) {
+            if (players[i] >= (server * m)) {
+                count += k;
+                server++;
+                answer++;
+            }
+            if (count > 0 ) count --;
+            if (count == 0) {
+                server--;
+            }
+        }
+        
         return answer;
     }
 }
