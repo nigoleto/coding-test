d38695e98eb3d3ea48be35dd7ace15a25d7e0d11
nigoleto <nigoleto@gmail.com>
2025-04-17 22:33:01 +0900

    프로그래머스_2_점프와 순간이동

--- 프로그래머스/Lv2/점프와 순간이동/Solution.java
+++ 프로그래머스/Lv2/점프와 순간이동/Solution.java
@@ -3,20 +3,17 @@ import java.util.*;
 public class Solution {
     public int solution(int n) {
         int ans = 0;
+        
+        while(n > 0) {
+            if(n % 2 == 0) {
+                n/=2;
+            } else {
+                n-=1;
+                ans++;
+            }
+        }
+        
 
-        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-        System.out.println("Hello Java");
-
-        //1
-        
-        // 2
-        // 4
-        // 8
-        // 16
-        // 15를 간다고 치면 1치고 4에서 3더가고 14에서 1더가고
-        
-        
-        
         return ans;
     }
 }
